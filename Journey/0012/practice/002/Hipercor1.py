4cd1dd10b5a85f91be9d568b68583f3698451a4f
akimwong <91024564+akimwong@users.noreply.github.com>
2022-11-14 16:31:23 +0100

    Update Hipercor1.py

--- Journey/0012/practice/002/Hipercor1.py
+++ Journey/0012/practice/002/Hipercor1.py
@@ -8,7 +8,14 @@ soup = bs(page.content, 'html.parser')
 
 containers = soup.find_all('div',class_="grid-item")
 len(containers)
+# 24
 
 containers[1].find_all('a',class_='link event js-product-link')[0].text
+#'GRAN CUK pollo Gran Cuk criado con alimentación vegetal pechuga entera bandeja 450 g peso aproximado '
 containers[1].find_all('div',class_='prices-price _current')[0].text
+#'5,69 €'
+'''
+It is not developed yet the code to create the dataset because ''prices-price _pum' does not exist for all products
+'''
 containers[1].find_all('div',class_='prices-price _pum')[0].text
+#'(12,65 € / Kg)'
